bb6392f200a1eb36f2dde533aae758a292ae78d0
Adriano Santos <adrianosantosdi@gmail.com>
2023-12-16 03:03:16 -0300

    Concluido a tela de cadastro e a função de salvar o produto

---
+++ unitDM.pas
@@ -0,0 +1,38 @@
+unit unitDM;
+
+interface
+
+uses
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MySQL,
+  FireDAC.Phys.MySQLDef, FireDAC.VCLUI.Wait, FireDAC.Stan.Param, FireDAC.DatS,
+  FireDAC.DApt.Intf, FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet,
+  FireDAC.Comp.Client;
+
+type
+  TDM = class(TDataModule)
+    FDConn: TFDConnection;
+    QryProdutos: TFDQuery;
+    dsProdutos: TDataSource;
+    FDPhysMySQLDriverLink1: TFDPhysMySQLDriverLink;
+    QryProdutosid: TFDAutoIncField;
+    QryProdutosdescricao: TStringField;
+    QryProdutosean: TIntegerField;
+    QryProdutospreco_venda: TBCDField;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  DM: TDM;
+
+implementation
+
+{%CLASSGROUP 'Vcl.Controls.TControl'}
+
+{$R *.dfm}
+
+end.
